6dd4cad12fded35f09a5b13466e49cddad194caa
Ace for Safu <safuh8@gmail.com>
2026-08-26 05:35:47 -0400

    feat: expose domain models and validation

--- src/routemind/domain/__init__.py
+++ src/routemind/domain/__init__.py
@@ -1,1 +1,43 @@
 """Core RouteMind domain models."""
+
+from .models import (
+    Location,
+    OptimizationPolicy,
+    OptimizationResult,
+    Package,
+    PricingModel,
+    Shipment,
+    TransportCapacity,
+    TransportLeg,
+    TransportMode,
+    TransportOption,
+    TransportPlan,
+    TransportPrice,
+    TransportSchedule,
+)
+from .validation import (
+    schedule_is_available_after,
+    schedule_is_valid,
+    shipment_timing_is_valid,
+    transport_can_carry_shipment,
+)
+
+__all__ = [
+    "Location",
+    "OptimizationPolicy",
+    "OptimizationResult",
+    "Package",
+    "PricingModel",
+    "Shipment",
+    "TransportCapacity",
+    "TransportLeg",
+    "TransportMode",
+    "TransportOption",
+    "TransportPlan",
+    "TransportPrice",
+    "TransportSchedule",
+    "schedule_is_available_after",
+    "schedule_is_valid",
+    "shipment_timing_is_valid",
+    "transport_can_carry_shipment",
+]
