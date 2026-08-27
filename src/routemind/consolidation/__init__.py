2631237703a56c0a28f23019be3dd299ca614feb
Ace for Safu <safuh8@gmail.com>
2026-08-27 06:22:27 -0400

    feat: expose shipment grouping primitives

--- src/routemind/consolidation/__init__.py
+++ src/routemind/consolidation/__init__.py
@@ -1,6 +1,12 @@
 """Deterministic shipment consolidation primitives."""
 
 from .engine import detect_shared_segments, evaluate_consolidation
+from .grouping import (
+    candidate_paths_by_shipment,
+    generate_consolidation_groups,
+    generate_shipment_groups,
+    reject_unknown_shipments,
+)
 from .models import (
     ConsolidationOpportunity,
     ConsolidationRejection,
@@ -13,6 +19,10 @@ __all__ = [
     "ConsolidationRejection",
     "ConsolidationRejectionReason",
     "SharedTransportSegment",
+    "candidate_paths_by_shipment",
     "detect_shared_segments",
     "evaluate_consolidation",
+    "generate_consolidation_groups",
+    "generate_shipment_groups",
+    "reject_unknown_shipments",
 ]
