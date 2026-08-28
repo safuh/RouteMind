edf074574aa0387cfde6d5e344a2f9cdc744de90
Ace for Safu <safuh8@gmail.com>
2026-08-28 10:11:02 -0400

    feat: expose hub-and-spoke opportunity discovery

--- src/routemind/consolidation/__init__.py
+++ src/routemind/consolidation/__init__.py
@@ -8,6 +8,7 @@ from .grouping import (
     generate_shipment_groups,
     reject_unknown_shipments,
 )
+from .hub_spoke import common_prefix_option_ids, generate_hub_spoke_groups
 from .models import (
     ConsolidationOpportunity,
     ConsolidationRejection,
@@ -38,9 +39,11 @@ __all__ = [
     "candidate_paths_by_shipment",
     "check_group_time_windows",
     "check_path_time_window",
+    "common_prefix_option_ids",
     "detect_shared_segments",
     "evaluate_consolidation",
     "generate_consolidation_groups",
+    "generate_hub_spoke_groups",
     "generate_shipment_groups",
     "reject_unknown_shipments",
     "reserve_opportunities",
