948e6a5ac35dc7c49f68ed59ad12aa8b765bfe33
Ace for Safu <safuh8@gmail.com>
2026-08-28 19:51:16 -0400

    style: sort graph package imports

--- src/routemind/graph/__init__.py
+++ src/routemind/graph/__init__.py
@@ -1,6 +1,6 @@
 """Time-dependent multimodal transportation graph."""
 
-from .models import GraphEdge, GraphNode, TransportGraph
 from .builder import build_transport_graph
+from .models import GraphEdge, GraphNode, TransportGraph
 
 __all__ = ["GraphEdge", "GraphNode", "TransportGraph", "build_transport_graph"]
